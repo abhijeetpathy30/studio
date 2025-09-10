1a64c1aaff1aecad1a888a9d98798401a9e74d68
Abhijeet Pathy <abhijeetpathy30@gmail.com>
2025-09-10 17:38:14 +0000

    fix this bug

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+
+import { MetadataRoute } from 'next'
+ 
+const baseUrl = 'https://studio-khaki-three.vercel.app';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
