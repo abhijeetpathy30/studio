e6cf2ad1b31dcde5f67d4da6fcfc6315b27ccbb8
Abhijeet Pathy <abhijeetpathy30@gmail.com>
2025-09-10 05:11:36 +0000

    improve the color contrast between the header and top section of the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
           <div className="flex-grow">
             {children}
           </div>
-          <footer className="py-12 text-center text-sm text-muted-foreground border-t bg-secondary/50">
+          <footer className="py-12 text-center text-sm text-muted-foreground border-t bg-secondary">
             <div className="container mx-auto px-4">
                 <p className="mb-4">Developed by Abhijeet Pathy</p>
                 <a href="https://www.linkedin.com/in/abhijeet-pathy-165b75118/" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 hover:text-primary transition-colors">
